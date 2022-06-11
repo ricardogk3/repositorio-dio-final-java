ee75a2d74030769357ba183e3fc6d3034ff895a1
Ricardo Kreutzer <rgk19@hotmail.com>
2022-06-11 20:17:50 -0300

    first commit

---
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -0,0 +1,32 @@
+package me.dio.academia.digital.controller;
+
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    @Autowired
+    private AlunoServiceImpl service;
+
+    @GetMapping
+    public List<Aluno> getAll(){
+        return service.getAll();
+    }
+
+    @PostMapping
+    public Aluno create(@RequestBody AlunoForm form){
+        return service.create(form);
+    }
+
+    @GetMapping("/avaliacoes/{id}")
+    public List<AvaliacaoFisica> getAllAvaliacaoFisicaId(@PathVariable Long id){
+        return service.getAllAvalicacaoFisicaId(id);
+    }
+
+}
---
+++ src/main/java/me/dio/academia/digital/controller/AvaliacaoFisicaController.java
@@ -0,0 +1,24 @@
+package me.dio.academia.digital.controller;
+
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/avaliacoes")
+public class AvaliacaoFisicaController {
+
+
+    @Autowired
+    private AvaliacaoFisicaServiceImpl service;
+
+    @PostMapping
+    public AvaliacaoFisica create(@RequestBody AvaliacaoFisicaForm form){
+        return service.create(form);
+    }
+
+
+
+}
